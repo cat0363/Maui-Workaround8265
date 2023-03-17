6a1f40af29b3e3d06ba8005228e740e5bafc2a3d
cat0363 <kenji.nagano.0363@gmail.com>
2023-03-17 17:48:24 +0900

    Workaround code of #8265

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Maui_Workaround8265;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Platforms/Android/CustomPickerHandler.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Maui.Handlers;
+using Microsoft.Maui.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maui_Workaround8265.Platforms.Android
+{
+    public class CustomPickerHandler : PickerHandler
+    {
+        /// <summary>
+        /// Create Platform View
+        /// </summary>
+        /// <returns>Platform View</returns>
+        protected override MauiPicker CreatePlatformView()
+        {
+            // Create Platform View
+            var platformView = base.CreatePlatformView();
+            // Initialize Key Listener
+            platformView.KeyListener = null;
+
+            return platformView;
+        }
+
+        /// <summary>
+        /// Connect Handler
+        /// </summary>
+        /// <param name="platformView">Platform View</param>
+        protected override void ConnectHandler(MauiPicker platformView)
+        {
+            base.ConnectHandler(platformView);
+        }
+
+        /// <summary>
+        /// Disconnect Handler
+        /// </summary>
+        /// <param name="platformView">Platform View</param>
+        protected override void DisconnectHandler(MauiPicker platformView)
+        {
+            base.DisconnectHandler(platformView);
+        }
+    }
+}
